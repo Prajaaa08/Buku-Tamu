e620581fde4907305c512ae0822b9a00999c9d51
Prajaaa08 <lestariaagustina@gmail.com>
2024-10-01 09:00:43 +0700

    menambahkan halaman laporan dan fitur tampil laporan

--- laporan.php
+++ laporan.php
@@ -52,7 +52,46 @@ include_once('templates/header.php');
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Tanggal</th>
+                            <th>Nama Tamu</th>
+                            <th>Alamat</th>
+                            <th>No. Telp/HP</th>
+                            <th>Bertemu Dengan</th>
+                            <th>kepentingan</th>
+                            <th>Aksi</th>
+                        </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if (isset($_POST['tampilkan'])) {
+                        $p_awal = $_POST['p_awal'];
+                        $p_akhir = $_POST['p_akhir'];
+                        // penomoran auto-increment
+                        $no = 1;
+                        // query untuj memangil semua data dari tabel buku_tamu
+                        $buku_tamu = query("SELECT * FROM buku_tamu WHERE tanggal BETWEEN '$p_awal' AND '$p_akhir' ");
+                        foreach ($buku_tamu as $tamu) : ?>
+                        <tr>
+                            <td><?= $no++; ?></td>
+                            <td><?= $tamu['tanggal']?></td>
+                            <td><?= $tamu['nama_tamu']?></td>
+                            <td><?= $tamu['alamat']?></td>
+                            <td><?= $tamu['no_hp']?></td>
+                            <td><?= $tamu['bertemu']?></td>
+                            <td><?= $tamu['kepentingan']?></td>
+                            <td>
+                                <a href="edit-tamu.php?id=<?=$tamu['id_tamu'] ?>" class="btn btn-success">Ubah</a>
+                                <a onclick="return confirm('Apakah anda yakin ingin mengahapus data ini?')" class="btn btn-danger" href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
                 
+                            </td>
+                        </tr>
+                <?php endforeach;
+                    }
+                    ?>    
+                </tbody>
             </table>
         </div>
     </div>
